9c392e83342ac59fb1cea29a7867cf4d90b944ff
kumatheworld <kum4th3w0rld@gmail.com>
2023-06-26 22:03:44 +0900

    Update set_seed()

--- fakeglyph/utils/seed.py
+++ fakeglyph/utils/seed.py
@@ -3,14 +3,20 @@ import random
 
 import numpy as np
 import torch
-import torch.backends.cudnn as cudnn
+from torch.backends import cudnn
 
 
 def set_seed(seed: int) -> None:
+    """
+    Not entirely sure if this works properly.
+    See https://pytorch.org/docs/stable/notes/randomness.html
+    and https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+    for details.
+    """
     os.environ["PYTHONHASHSEED"] = str(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    cudnn.deterministic = True
+    torch.use_deterministic_algorithms(True)
     cudnn.benchmark = False
